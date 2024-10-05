19bb6c75705dbd3d73caba3e9e04fd8db21b2eec
AbooMinister25 <aboominister@gmail.com>
2024-10-05 18:27:16 -0400

    Change nixos icon for launcher button

--- home/ags/widgets/bar/launcher.js
+++ home/ags/widgets/bar/launcher.js
@@ -2,7 +2,7 @@ export const Launcher = () => Widget.Box({
     class_name: "bar-applet",
     children: [Widget.Button({
         child: Widget.Icon({
-            icon: "nix-snowflake-symbolic",
+            icon: "nix-snowflake-custom-symbolic",
             class_name: "launcher-icon",
         }),
         onClicked: () => Utils.execAsync("rofi -show drun -modi 'exp:~/.config/rofi/scripts/expand.sh' -kb-custom-1 'ctrl+x' -theme ~/.config/rofi/style-6.rasi"),
